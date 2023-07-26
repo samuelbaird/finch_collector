c38797871615d1a1b1589c161a4443d71bcfd3bd
Samuel Baird <sam@samuelbaird.com>
2023-07-26 16:46:45 -0300

    detail page and refactor links

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Finch(models.Model):
+    common_name = models.CharField(max_length=100, default='')
+    scientific_name = models.CharField(max_length=100, default='')
+    beak_type = models.CharField(max_length=100, default='')
+    food = models.CharField(max_length=100, default='')
+    image = models.CharField(max_length=500, default='')
+
+    def __str__(self):
+        return self.common_name
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Finch
 
 finches = [
   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
@@ -14,6 +15,13 @@ def about (request):
     return render(request, 'about.html')
 
 def finches_index (request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
+    })
+
+def finches_detail (request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch
     })
