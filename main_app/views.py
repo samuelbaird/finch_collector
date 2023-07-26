5ba46a8b2282ff2f95dc419dd5cf91e49a211969
Samuel Baird <sam@samuelbaird.com>
2023-07-26 14:15:42 -0300

    view all page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 
 def home (request):
@@ -7,3 +12,8 @@ def home (request):
 
 def about (request):
     return render(request, 'about.html')
+
+def finches_index (request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
