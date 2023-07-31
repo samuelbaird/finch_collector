2955c975172163ac7722473e90319bc22f08ddf4
Samuel Baird <sam@samuelbaird.com>
2023-07-31 12:07:19 -0300

    many to many

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
     path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
     path('finches/<int:finch_id>/add_sighting/', views.add_sighting, name='add_sighting'),
+    path('finches/<int:finch_id>/assoc_food/<int:food_id>/', views.assoc_food, name='assoc_food'),
+    path('finches/<int:finch_id>/unassoc_food/<int:food_id>/', views.unassoc_food, name='unassoc_food'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from .models import Finch, Sighting, Food
 from .forms import SightingForm
 
 finches = [
@@ -24,18 +24,21 @@ def finches_index (request):
 
 def finches_detail (request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    id_list = finch.food.all().values_list('id')
+    food_finch_doesnt_eat = Food.objects.exclude(id__in = id_list)
     sighting_form = SightingForm()
     return render(request, 'finches/detail.html', {
-        'finch': finch, 'sighting_form': sighting_form
+        'finch': finch, 'sighting_form': sighting_form,
+        'food' : food_finch_doesnt_eat
     })
 
 class FinchCreate(CreateView):
     model = Finch
-    fields = '__all__'
+    fields = ['common_name', 'scientific_name', 'beak_type',]
 
 class FinchUpdate(UpdateView):
     model = Finch
-    fields = ['common_name', 'scientific_name', 'beak_type', 'food']
+    fields = ['common_name', 'scientific_name', 'beak_type',]
 
 class FinchDelete(DeleteView):
     model = Finch
@@ -47,4 +50,12 @@ def add_sighting(request, finch_id):
         new_sighting = form.save(commit=False)
         new_sighting.finch_id = finch_id
         new_sighting.save()
+    return redirect('detail', finch_id=finch_id)
+
+def assoc_food(request, finch_id, food_id):
+    Finch.objects.get(id=finch_id).food.add(food_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_food(request, finch_id, food_id):
+    Finch.objects.get(id=finch_id).food.remove(food_id)
     return redirect('detail', finch_id=finch_id)
